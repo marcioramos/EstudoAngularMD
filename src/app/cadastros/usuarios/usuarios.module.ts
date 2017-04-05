58c9465abad729d2084e4fac439b5925984d531d
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-04-04 23:19:34 -0300

    Continuando construir a base do sistema

--- src/app/cadastros/usuarios/usuarios.module.ts
+++ src/app/cadastros/usuarios/usuarios.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsuarioListaComponent } from './usuario-lista/usuario-lista.component';
+import { UsuarioEditComponent } from './usuario-edit/usuario-edit.component';
+import { UsuarioNovoComponent } from './usuario-novo/usuario-novo.component';
+import { UsuarioDetalheComponent } from './usuario-detalhe/usuario-detalhe.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [UsuarioListaComponent, UsuarioEditComponent, UsuarioNovoComponent, UsuarioDetalheComponent]
 })
 export class UsuariosModule { }
